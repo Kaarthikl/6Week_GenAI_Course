37c6da50b5e3b10cc71f2d59187ddf467d9415b4
Kaarthikl <kaarthikl@gmail.com>
2026-05-14 19:51:30 +0530

    Add files via upload
    Python Learning for Day 3

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,16 @@
+#Addition
+a=20
+b=15
+print(a+b)
+#Subtraction
+print(a-b)
+#Multiplication
+print(a*b)
+#Division
+print(a/b)
+#Floor Division
+print(a//b)
+#Modulus
+print(a%b)
+#Exponentiation
+print(a**b)
